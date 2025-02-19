1c1050758d56082b96cdab762f766f44e98249d6
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2025-02-19 20:39:08 +0000

    se modifica login y private

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -8,6 +8,8 @@ import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 import { SingUp } from "./pages/singup.jsx";
+import { Login } from "./pages/login.jsx";
+import { Private } from "./pages/private.jsx";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
@@ -30,6 +32,8 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<SingUp />} path="/singup" />
+                        <Route element={<Login />} path="/login" />
+                        <Route element={<Private />} path="/private" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />

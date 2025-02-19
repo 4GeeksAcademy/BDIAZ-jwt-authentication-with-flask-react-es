35be6db6931a0649bca830358cdcdb8daf1d165a
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2025-02-19 19:20:58 +0000

    se agrega registro y pagina

---
+++ src/front/js/pages/singup.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { Context } from "../store/appContext";
+
+export const SingUp = () => {
+    const { store, actions } = useContext(Context);
+    const [data, setData] = useState({
+		email: "",
+		password: ""
+	});
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setData((prevData) => ({
+			...prevData,
+			[name]: value
+		}));
+	};
+    const handleSingUp = async (e) => {
+		e.preventDefault();
+        console.log("data.email", data.email)
+        console.log("data.password", data.password)
+		// const result = await actions.singUp(data.email, data.password);
+		// if (result) {
+		// 	navigate("/protected_route");
+		// } else {
+		// 	alert("Error logging in");
+		// }
+	};
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <form onSubmit={handleSingUp}>
+                        <div className="mb-3">
+                            <label htmlFor="exampleInputEmail1" className="form-label">Ingrese su correo</label>
+                            <input type="email" 
+                                    className="form-control" 
+                                    id="exampleInputEmail1" 
+                                    aria-describedby="emailHelp"
+                            		value={data.email}
+                                    onChange={handleChange}
+                                    name="email"
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                            <input type="password" 
+                                    className="form-control" 
+                                    id="exampleInputPassword1"
+                                    value={data.password}
+                                    onChange={handleChange}
+                                    name="password"
+                                    />
+                        </div>                        
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+
+                </div>
+            </div>
+            <br />
+            <Link to="/">
+                <button className="btn btn-primary">Back home</button>
+            </Link>
+        </div>
+    );
+};
